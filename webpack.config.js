f44cc81749be19da2b0c7e581b65963d7b550800
Ramon3162 <ramonomondi@gmail.com>
2018-11-22 12:35:12 +0300

    Configure webpack accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 
 module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index.bundle.js'
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
+                exclude: '/node_modules/',
+                use: 'babel-loader',
+            },
+            {
+                test: /\.jsx$/,
+                exclude: '/node_modules/',
+                use: 'babel-loader',
+            },
         ]
     },
-    plugins: [htmlPlugin]
-};
+    plugins: [HtmlWebpackPluginConfig],
+}
